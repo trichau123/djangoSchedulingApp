ccf01abd9ddcdfe325adcd5d20a2c58303c01b68
Chau Huynh Tri <htri7878@gmail.com>
2022-06-29 12:02:40 -0400

    jun 29 commit

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
 
 path("", views.lgin, name = "lgin"),
 path("index/", views.index, name = "index"),
-path("home/", views.pSchedule, name = "pSchedule"),
+path("pSchedule/", views.pSchedule, name = "pSchedule"),
+path("signin/", auth_views.LoginView.as_view(template_name= 'signin.html'), name = "signin"),
+path("signout/", auth_views.LogoutView.as_view(), name = "signout"),
+
 
 ]
 #start
--- main/views.py
+++ main/views.py
@@ -1,12 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+#check user and pass
+from django.contrib.auth import authenticate,  login,logout
+from django.contrib import messages
+#create user
+#from django.contrib.auth import User
 # Create your views here.
 
 def index(response):
-	return HttpResponse("<h1> login </h1>")
+	return HttpResponse("<h1> Welcome </h1>")
 
 def pSchedule(response):
 	return HttpResponse("<h1> schedule page </h1>")
 
 def lgin(request):
 	return render(request, 'index.html',{})
+
+#connect with index html
+def signin(request):
+
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+
+		user = authenticate(request, username=username, password=password)
+
+		if user is  not None:
+			login(request, user)
+			redirect( 'pSchedule')
+
+		else:
+			messages.error(request, "Bad Credentials")
+			return  redirect('')
+
+	return render(request,"signin.html")
+
